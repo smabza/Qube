f0420c322acbf0e73035536e89ed6a85abb252fb
Joonas Aaltonen <Joonas.e.aaltonen@hotmail.com>
2018-01-25 14:16:56 +0200

    Added return to home functionality to exit button

--- public/js/results.js
+++ public/js/results.js
@@ -17,7 +17,7 @@ catch (error)
     app.console.log(error);
 }
 
-var log = fs.readFileSync('public/logs/data.txt', 'utf8');
+var log = fs.readFileSync('public/logs/data1.txt', 'utf8');
 
 // Cut off all unnecessary stuff from the beginning until "Da, ... which is date object"
 var start = "Da";
@@ -154,4 +154,11 @@ function showResults(resultKeys, elementId)
             index++;
         }
     }
+}
+
+// // EXIT BUTTON
+function goHome()
+{
+    window.location = "http://127.0.0.1:3000/";
+    app.console.log("WAAWW")
 }
