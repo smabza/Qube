94bfa2e7560bd42b91fca4e12db428d8eecba9f4
smabza <32483300+smabza@users.noreply.github.com>
2018-02-27 18:11:51 +0200

    Update fi_descriptions.js

--- public/logs/fi_descriptions.js
+++ public/logs/fi_descriptions.js
@@ -24,46 +24,46 @@ var fiDescriptions =
     "OV": "Ylipainon aste",
     "Sf": "Standard fat %",
     "SM": "Standard muscle mass",
-    "IF": "Sisäelinrasva",
-    "LP": "Alaraajojen lihasten taso",
-    "rB": "Perusaineenvaihdunta kcal",
-    "rb": "BMR kJ",
-    "rJ": "Perusaineenvaihdunta taso",
-    "rA": "Metabolinen ikä",
+    "IF": "Sisäelinrasva", "Rasva, joka on kertynyt tärkeiden sisäelinten ympärille. Normaali taso on 1 - 12 välillä ja huono taso on 12 - 59 välillä."
+    "LP": "Alaraajojen lihasten taso", "Alaraajojen lihasten taso kertoo, kuinka vahvat alaraajat ovat. Matala taso on -4 ja -2 välillä, keskitaso -2 ja 2 välillä ja korkea taso 2 ja 4 välillä."
+    "rB": "Perusaineenvaihdunta kcal", "Perusaineenvaihdunnalla tarkoitetaan välttämättömien elintoimintojen aiheuttamaa energiankulutusta kilokaloreissa."
+    "rb": "BMR kJ", "BMR:llä tarkoitetaan välttämättömien elintoimintojen aiheuttamaa energian kulutusta kilojouleissa."
+    "rJ": "Perusaineenvaihdunta taso", "Perusaineenvaihdunta tasolla tarkoitetaan välttämättömien elintoimintojen aiheuttamaa energiankulutusta tasolla a – b."
+    "rA": "Metabolinen ikä", "Metabolinen ikä vertaa mitattua perusaineenvaihduntaa ikäryhmäsi keskimääräiseen perusaineenvaihduntaan. Jos metabolinen ikä on pienempi kuin oikea ikäsi, niin se viittaa siihen, että kehosi on hyvässä kunnossa verrattuna ikäryhmääsi. Metabolisen iän ollessa suurempi oikeaa ikääsi viittaa siihen, että kehosi on heikommassa kunnossa verrattuna ikäryhmääsi."
     "BA": "Muscle mass balance(arm)",
     "BF": "Muscle mass balance(leg)",
     "gF": "Target body fat %",
     "gW": "Predicted weight",
     "gf": "Predicted fat mass",
     "gt": "Fat to lose/gain",
-    "FR": "Oikean jalan rasvaprosentti",
-    "fR": "Oikean jalan rasvamassa",
-    "MR": "Right Leg Fat free mass",
-    "mR": "Oikean jalan lihasmassa",
+    "FR": "Oikean jalan rasvaprosentti", "Rasvamassa suhteessa muuhun massaan oikeassa jalassa."
+    "fR": "Oikean jalan rasvamassa", "Rasvan määrä kilogrammoina (kg) oikeassa jalassa"
+    "MR": "Right Leg Fat free mass", 
+    "mR": "Oikean jalan lihasmassa", "Lihaksen määrä kilogrammoina (kg) oikeassa jalassa"
     "SR": "Right Leg Fat % score",
     "sR": "Right Leg Muscle mass score",
-    "FL": "Vasemman jalan rasvaprosentti",
-    "fL": "Vasemman jalan rasvamassa",
+    "FL": "Vasemman jalan rasvaprosentti", "Rasvamassa suhteessa muuhun massaan vasemmassa jalassa."
+    "fL": "Vasemman jalan rasvamassa", "Rasvan määrä kilogrammoina (kg) vasemmassa jalassa."
     "ML": "Left Leg Fat free mass",
-    "mL": "Vasemman jalan lihasmassa",
+    "mL": "Vasemman jalan lihasmassa","Lihaksen määrä kilogrammoina (kg) vasemmassa jalassa."
     "SL": "Left Leg Fat % score",
     "sL": "Left Leg Muscle mass score",
-    "Fr": "Oikean käden rasvaprosentti",
-    "fr": "Oikean käden rasvamassa",
+    "Fr": "Oikean käden rasvaprosentti", "Rasvamassa suhteessa muuhun massaan oikeassa kädessä."
+    "fr": "Oikean käden rasvamassa", "Rasvan määrä kilogrammoina (kg) oikeassa kädessä."
     "Mr": "Right Arm Fat free mass",
-    "mr": "Oikean käden lihasmassa",
+    "mr": "Oikean käden lihasmassa", "Lihaksen määrä kilogrammoina (kg) oikeassa kädessä."
     "Sr": "Right Arm Fat % score",
     "sr": "Right Arm Muscle mass score",
-    "Fl": "Vasemman käden rasvaprosentti",
-    "fl": "Vasemman käden rasvamassa",
+    "Fl": "Vasemman käden rasvaprosentti", "Rasvamassa suhteessa muuhun massaan vasemmassa kädessä."
+    "fl": "Vasemman käden rasvamassa", "Rasvan määrä kilogrammoina (kg) vasemmassa kädessä."
     "Ml": "Left Arm Fat free mass",
-    "ml": "Vasemman käden lihasmassa",
+    "ml": "Vasemman käden lihasmassa", "Lihaksen määrä kilogrammoina (kg) vasemmassa kädessä."
     "Sl": "Left Arm Fat % score",
     "sl": "Left Arm Muscle mass score",
-    "FT": "Keskivartalon rasvaprosentti",
-    "fT": "Keskivartalon rasvamassa",
+    "FT": "Keskivartalon rasvaprosentti", "Rasvamassa suhteessa muuhun massaan keskivartalossa."
+    "fT": "Keskivartalon rasvamassa", "Rasvan määrä kilogrammoina (kg) keskivartalossa."
     "MT": "Trunk Fat free mass",
-    "mT": "Keskivartalon lihasmassa",
+    "mT": "Keskivartalon lihasmassa", "Lihaksen määrä kilogrammoina (kg) keskivartalossa."
     "ST": "Trunk Fat % score",
     "sT": "Trunk Muscle mass score",
     "CS": "Check sum"  
