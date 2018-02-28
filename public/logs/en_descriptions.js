93ef5db37e90eeca46062b1a5a158b24b5e2b63c
smabza <32483300+smabza@users.noreply.github.com>
2018-02-28 19:33:27 +0200

    Update en_descriptions.js

--- public/logs/en_descriptions.js
+++ public/logs/en_descriptions.js
@@ -24,46 +24,46 @@ var enDescriptions =
     "OV": "Degree of obesity",
     "Sf": "Standard fat %",
     "SM": "Standard muscle mass",
-    "IF": "Visceral fat rating",
-    "LP": "Leg muscle score",
-    "rB": "Basic Metabolic Rate kcal",
-    "rb": "BMR kJ",
-    "rJ": "Basic Metabolic Rate score",
-    "rA": "Metabolic age",
+    "IF": "Visceral fat rating", "Visceral fat is wrapped around major organs like the liver, pancreas and your kidney. Healthy rating is 1 - 12 and excessive is 12 - 59."
+    "LP": "Leg muscle score", 
+    "rB": "Basic Metabolic Rate kcal", "It is the amount of energy(kcal) your body burns at rest on a daily basis."
+    "rb": "BMR kJ", "It is the amount of energy(kJ) your body burns at rest on a daily basis."
+    "rJ": "Basic Metabolic Rate score", "It is the amount of energy your body burns at rest on a daily basis."
+    "rA": "Metabolic age", "Metabolic age compares your Basal Metabolic Rate to the average BMR of your age group. If your metabolic age is smaller than your age, it's a sign that you body is in better shape than average of your age group. If your metabolic age is higher than your actual age, it’s an sign that you need to improve your metabolic rate."
     "BA": "Muscle mass balance(arm)",
     "BF": "Muscle mass balance(leg)",
     "gF": "Target body fat %",
     "gW": "Predicted weight",
     "gf": "Predicted fat mass",
     "gt": "Fat to lose/gain",
-    "FR": "Right Leg Fat %",
-    "fR": "Right Leg Fat mass",
+    "FR": "Right Leg Fat %", "It is the proportion of fat in the right leg"
+    "fR": "Right Leg Fat mass", "It is the actual weight of fat in the right leg"
     "MR": "Right Leg Fat free mass",
-    "mR": "Right Leg Muscle mass",
+    "mR": "Right Leg Muscle mass", "It is the actual weight of muscle in the right leg"
     "SR": "Right Leg Fat % score",
     "sR": "Right Leg Muscle mass score",
-    "FL": "Left Leg Fat %",
-    "fL": "Left Leg Fat mass",
+    "FL": "Left Leg Fat %", "It is the proportion of fat in the left leg"
+    "fL": "Left Leg Fat mass","It is the actual weight of fat in the left leg"
     "ML": "Left Leg Fat free mass",
-    "mL": "Left Leg Muscle mass",
+    "mL": "Left Leg Muscle mass","It is the actual weight of muscle in the left leg"
     "SL": "Left Leg Fat % score",
     "sL": "Left Leg Muscle mass score",
-    "Fr": "Right Arm Fat %",
-    "fr": "Right Arm Fat mass",
+    "Fr": "Right Arm Fat %", "It is the proportion of fat in the right arm"
+    "fr": "Right Arm Fat mass","It is the actual weight of fat in the right arm"
     "Mr": "Right Arm Fat free mass",
-    "mr": "Right Arm Muscle mass",
+    "mr": "Right Arm Muscle mass","It is the actual weight of muscle in the right arm"
     "Sr": "Right Arm Fat % score",
     "sr": "Right Arm Muscle mass score",
-    "Fl": "Left Arm Fat %",
-    "fl": "Left Arm Fat mass",
+    "Fl": "Left Arm Fat %","It is the proportion of fat in the left arm"
+    "fl": "Left Arm Fat mass","It is the actual weight of fat in the left arm"
     "Ml": "Left Arm Fat free mass",
-    "ml": "Left Arm Muscle mass",
+    "ml": "Left Arm Muscle mass","It is the actual weight of muscle in the left arm"
     "Sl": "Left Arm Fat % score",
     "sl": "Left Arm Muscle mass score",
-    "FT": "Trunk Fat %",
-    "fT": "Trunk Fat mass",
+    "FT": "Trunk Fat %","It is the proportion of fat in the trunk"
+    "fT": "Trunk Fat mass","It is the actual weight of fat in the trunk"
     "MT": "Trunk Fat free mass",
-    "mT": "Trunk Muscle mass",
+    "mT": "Trunk Muscle mass","It is the actual weight of muscle in the trunk"
     "ST": "Trunk Fat % score",
     "sT": "Trunk Muscle mass score",
     "CS": "Check sum"  
